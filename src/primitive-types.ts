11ce9ab4a52bfe3ea724ff7bd3839200ed3d250e
petia duskin <petr.dyuskin@mail.ru>
2024-10-04 21:40:46 +0500

    add primitive types

---
+++ src/primitive-types.ts
@@ -0,0 +1,9 @@
+let isPresent: boolean = false;
+let magic: number = 51.1;
+let str: string = 'Hello world'
+
+let notDefined: undefined = undefined;
+let notPresent: null = null;
+
+let id: symbol = Symbol('id')
+let bigNumber: bigint = 55n;

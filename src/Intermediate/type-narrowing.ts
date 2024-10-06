3dd689911fdd249e790875ad8d72ada794699c89
petia duskin <petr.dyuskin@mail.ru>
2024-10-06 21:03:32 +0500

    update type narrowing

--- src/Intermediate/type-narrowing.ts
+++ src/Intermediate/type-narrowing.ts
@@ -53,3 +53,54 @@ function area(shape: Shape) {
         return shape.width * shape.height;
     }
 }
+
+// либо мы можем добавить специальное отдельное свойство в тип, которое будет указывать на конкретный класс
+
+type Circle = {
+    kind: 'circle',
+    radius: 5
+}
+
+type RectanglE = {
+    kind: 'rectangle'
+    coordinates: [number, number][]
+}
+
+let arrt: RectanglE = {
+    kind: 'rectangle',
+    coordinates: [[0, 0], [1, 1], [2, 2]],
+}
+
+type Shape2 = Circle | RectanglE;
+
+function area2(shape: Shape2) {
+    if (shape.kind === 'circle') {
+        return (shape.radius ** 2) * 3.14
+    }
+
+    if (shape.kind === 'rectangle') {
+        return shape.coordinates
+    }
+}
+
+type ValidationSuccess = {
+    isValid: true;
+    validatedValue: string;
+}
+
+type ValidationFailure = {
+    isValid: false;
+    errorReason: string;
+}
+
+type ValidationResult = ValidationSuccess | ValidationFailure;
+
+function logResult(result: ValidationResult) {
+    if (result.isValid) {
+        console.log(result.validatedValue)
+    }
+
+    if (!result.isValid) {
+        console.log(result.errorReason)
+    }
+}
